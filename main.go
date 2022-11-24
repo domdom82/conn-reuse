2eeb1d99a62845d42b9547912c6c9acc417901bb
Dominik Froehlich <dominik.froehlich@sap.com>
2022-11-24 14:16:43 +0100

    write keylog files to decrypt traffic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net/http"
@@ -38,12 +39,25 @@ func req(client *http.Client, url string, prefix string, errors *int) {
 }
 
 func newConn(url string) {
+
+	newConnKeyLog, err := os.Create("newConn.keylog")
+
+	if err != nil {
+		panic(err)
+	}
+
+	tlsConfig := &tls.Config{
+		KeyLogWriter: newConnKeyLog,
+	}
+
 	var transport *http.Transport
 	errors := 0
 
 	for {
 		time.Sleep(1 * time.Second)
-		transport = &http.Transport{}
+		transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
 		client := &http.Client{Transport: transport}
 		req(client, url, "newConn", &errors)
 		transport.CloseIdleConnections()
@@ -53,8 +67,19 @@ func newConn(url string) {
 
 func reuseConn(url string) {
 
+	oldConnKeyLog, err := os.Create("oldConn.keylog")
+
+	if err != nil {
+		panic(err)
+	}
+
+	tlsConfig := &tls.Config{
+		KeyLogWriter: oldConnKeyLog,
+	}
+
 	transport := &http.Transport{
 		MaxConnsPerHost: 1,
+		TLSClientConfig: tlsConfig,
 	}
 	client := &http.Client{Transport: transport}
 	errors := 0
